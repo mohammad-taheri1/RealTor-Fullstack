ac381de5f2532af795e04dcb6c9be21bffdec3aa
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-20 03:10:44 +0430

    Some Front changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,19 +22,16 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+            <div class="container ">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+
+                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> --}}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
+                <div>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,13 +5,15 @@
             <a href="/projects/create">New Project</a>
         </div>
 
-        <ul>
+        <div class="flex">
             @forelse ($projects as $project)
-                <li>
-                    <a href="{{ $project->path() }}">{{ $project->title }}</a>
-                </li>
+                <div class="bg-white mr-4 p-4 rounded shadow w-1/3" style="height: 200px">
+                    <h3 class="font-normal text-xl mb-4  py-6"> {{ $project->title }}</h3>
+
+                    <div class="text-muted"> {{ Str::limit($project->description, 100) }} </div>
+                </div>
             @empty
-                <li>No projects yet.</li>
+                <div>No Projects yet.</div>
             @endforelse
-        </ul>
+        </div>
 @endsection
