36083d2b265462015c2a3b701a81c220c82d07e3
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-19 19:26:34 +0430

    update projects migration => pass first test

--- database/migrations/2021_08_19_142720_create_projects_table.php
+++ database/migrations/2021_08_19_142720_create_projects_table.php
@@ -15,6 +15,8 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
