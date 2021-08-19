3c0244c66ee702feca7c6c1242abc32329934706
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-20 02:21:03 +0430

    Add Tailwind to project

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
 
-        <h1>Birdboard</h1>
+@section('content')
+        <div class="flex items-center mb-3">
+            <a href="/projects/create">New Project</a>
+        </div>
 
         <ul>
             @forelse ($projects as $project)
@@ -19,5 +14,4 @@
                 <li>No projects yet.</li>
             @endforelse
         </ul>
-</body>
-</html>
+@endsection
