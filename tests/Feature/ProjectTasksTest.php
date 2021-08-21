049c62a8d617dbfde4aa65f68ba23cca9c921db8
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-21 22:46:49 +0430

    create new test fot tasks

--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -2,17 +2,27 @@
 
 namespace Tests\Feature;
 
+use App\Models\Project;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProjectTasksTest extends TestCase
 {
-   use RefreshDatabase;
+    use RefreshDatabase;
 
-   /** @test */
-   public function a_project_can_have_tasks()
-   {
-      //
-   }
+    /** @test */
+    public function a_project_can_have_tasks()
+    {
+        $this->withoutExceptionHandling();
+        
+        $this->signIn();
+
+        $project = Project::factory()->create(['owner_id' => auth()->id()]);
+
+        $this->post($project->path() . '/tasks', ['body' => 'Test task']);
+
+        $this->get($project->path())
+            ->assertSee('Test task');
+    }
 }
